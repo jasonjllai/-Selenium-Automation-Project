a7a3936b00a590388b69ca9ac3da0161b5ae8ff2
Jason Lai <jl.lai@outlook.com>
2017-07-26 21:36:12 +1200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PageObjectDemo;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPageObjectDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test10()
+        {
+            Pages.HomePage.Goto();
+            //Pages.HomePage.ClickLogin();
+            //Pages.LoginPage.Login("jl.lai@outlook.com", "");
+            Pages.HomePage.ClickMotor();
+            Pages.MotorPage.ClickSale();
+            Pages.CarPage.ClickCar();
+            Pages.CarPage.Search(3, 5, 7, "Leather seats");
+            Browser.Close();
+
+
+        }
+    }
+}
